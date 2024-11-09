a5186292c5aa679ffb2cf6abcad38975cfdedfac
megaokna43 <fitobar43@gmail.com>
2024-11-09 18:28:44 +0300

    добавил тесты, покрытие кода 90%

--- tests/test_load_json.py
+++ tests/test_load_json.py
@@ -4,33 +4,53 @@ from unittest.mock import mock_open, patch
 from src.utils import load_operations
 
 
-def load_json(file):
-    return json.load(file)
-
-
 def test_load():
-    op = load_operations('../operations.json')
-    # Проверка загрузки данный из файла json
+    op = load_operations('../operations.json') # Проверка загрузки данных из файла json
     assert len(op) >= 0
 
 
 def test_load_wrong():
-    op = load_operations('../wrong.json')
+    op = load_operations('../wrong.json') # Проверяю загрузку несуществующего файла
     assert len(op) == 0
 
-def test_load_json_valid():
-    mock_data = '{"name": "John", "age": 30}'
-    with patch('builtins.open', mock_open(read_data=mock_data)):
-        with open('operations.json') as file:
-            result = load_json(file)
-            assert result == {"name": "John", "age": 30}
 
-def test_load_json_invalid():
-    mock_data = '{"name": "John", "age": 30'  # Неправильный JSON
-    with patch('builtins.open', mock_open(read_data=mock_data)):
-        with pytest.raises(json.JSONDecodeError):
-            with open('operations.json') as file:
-                load_json(file)
+def test_load_operations_valid():
+    test_data = '''{
+        "id": 484201274,
+        "state": "EXECUTED",
+        "date": "2019-04-11T23:10:21.514616",
+        "operationAmount": {
+            "amount": "62621.51",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        }
+    }'''
 
-if __name__ == "__main__":
-    pytest.main()
+    with patch('builtins.open', mock_open(read_data=test_data)):
+        result = load_operations('operations.json')  # Call the function
+        expected_result = {
+            "id": 484201274,
+            "state": "EXECUTED",
+            "date": "2019-04-11T23:10:21.514616",
+            "operationAmount": {
+                "amount": "62621.51",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            }
+        }
+        assert result == expected_result  # Update the expected result
+
+
+def test_load_operations_file_not_found_error():
+    json_data = ''  # некорректный JSON
+    assert json_data != load_operations('operations.json')  # проверяем функцию
+
+def test_load_json_file_json_decode_error():
+    # Имитация открытия файла с некорректным JSON
+    mock_file = mock_open(read_data="{'key': 'value'")  # Неверный JSON (отсутствует закрывающая скобка)
+    with patch('builtins.open', mock_file):
+        assert load_operations('in_operations.json') == []
